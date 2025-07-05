1465d3257ec75decccae762994dced5e6a3a4dc6
oilcake <oilpie@gmail.com>
2025-07-05 19:00:31 +0300

    link extracted to its own file

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,12 @@ mod clip;
 mod run;
 mod state;
 mod vertex;
+mod link;
 
-use std::io::{self, Write};
+use link::Link;
 
 use ffmpeg_next as ffmpeg;
-use rusty_link::{AblLink, SessionState};
 
-struct Link {
-    link: AblLink,
-    quantum: f64,
-    beat: f64,
-    phase: f64,
-    state: SessionState,
-}
-impl Link {
-    fn new() -> Link {
-        // Link
-        let link = AblLink::new(120.0);
-        link.enable(true);
-        let state = SessionState::new();
-        let quantum = 1.0;
-        Link {
-            link,
-            quantum,
-            beat: 0.0,
-            phase: 0.0,
-            state,
-        }
-    }
-    fn update_phase_and_beat(&mut self) {
-        self.link.capture_app_session_state(&mut self.state);
-        let now = self.link.clock_micros();
-
-        self.beat = self.state.beat_at_time(now, self.quantum);
-        self.phase = self.state.phase_at_time(now, self.quantum);
-        print!("\rBeat: {:.2}, Phase: {:.2}", self.beat, self.phase);
-        let _ = io::stdout().flush();
-    }
-}
 fn main() {
     ffmpeg::init().unwrap();
     let filename = std::env::args()
