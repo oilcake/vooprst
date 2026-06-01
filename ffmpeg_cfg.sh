a2c37d0a77bbad89612f6b3a68e93c488f7b8445
oilcake <oilpie@gmail.com>
2026-06-02 01:05:27 +0300

    cargo/sandbox/ffmpeg confgigs

---
+++ ffmpeg_cfg.sh
@@ -0,0 +1,10 @@
+./configure --prefix=$HOME/code/voop/vooprst/ffmpeg \                                               ░▒▓ ✔ │ ▼ │ 00:42:57 
+            --enable-shared \
+            --disable-static \
+            --disable-debug \
+            --disable-videotoolbox \
+            --disable-metal \
+            --enable-libx264 \
+            --enable-gpl \
+            --extra-cflags="-I$HOME/local/ffmpeg/include" \
+            --extra-ldflags="-L$HOME/local/ffmpeg/lib"
